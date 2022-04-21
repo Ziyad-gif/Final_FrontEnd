bf2e364965523e4c84c29ec46ec262f5231a1209
Your Name <you@example.com>
2022-04-21 09:11:05 +0700

    done

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -13,43 +13,31 @@ function Form(){
   <form action="action_page.php">
     <h2>Form Covid</h2>
     <div class="row">
-      <div class="col-25">
-        <label for="kota">Kota</label>
-      </div>
-      <div class="col-75">
-        <input type="text" id="kota" name="kota" placeholder=""/>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-25">
-        <label for="sembuh">Sembuh</label>
-      </div>
-      <div class="col-75">
-        <input type="text" id="sembuh" name="Sembuh" placeholder=""/>
+      
+    <div class="col-75">
+        <select id="Provinsi" name="Provinsi">
+          <option value="Jakarta">Jakarta</option>
+          <option value="Bali">Bali</option>
+          <option value="JawaBarat">Jawa Barat</option>
+        </select>
       </div>
     </div>
     <div class="row">
-      <div class="col-25">
-        <label for="sakit">Sakit</label>
+    <div class="col-75">
+        <select id="Status" name="Status">
+          <option value="Sembuh">Sembuh</option>
+          <option value="Sakit">Sakit</option>
+          <option value="Meninggal"></option>
+        </select>
       </div>
-      <div class="col-75">
-        <input type="text" id="sakit" name="sakit" placeholder=""/>
-      </div>
+     
     </div>
     <div class="row">
       <div class="col-25">
-        <label for="Meninggal">Meninggal</label>
+        <label for="jumlah"></label>
       </div>
       <div class="col-75">
-        <input type="text" id="sembuh" name="Sembuh" placeholder=""/>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-25">
-        <label for="jumlah">Jumlah</label>
-      </div>
-      <div class="col-75">
-        <input type="text" id="jumlah" name="jumlah" placeholder=""/>
+        <input type="text" id="jumlah" name="jumlah" placeholder="Jumlah"/>
       </div>
     </div>
     <div class="row">
